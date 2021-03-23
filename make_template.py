cbe9538828e75a5d73aa0e90ffaecac9d8d8181c
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-23 22:12:04 +0530

    file name bug
    REMEMBER now working one more folder deep, so 3 times '../' not 2 times.

--- make_template.py
+++ make_template.py
@@ -1,5 +1,5 @@
 def make_template(URL: str):
-    with open("../../templates/py.py", mode="r") as tmp_file:
+    with open("../../../templates/py.py", mode="r") as tmp_file:
         boiler_plate = f"# URL: {URL}\n{tmp_file.read()}"
 
     with open("sol.py", mode="w") as sol_file:
