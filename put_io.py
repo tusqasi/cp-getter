2b3ed1d03d1a1430141a4e17f3db4bbe61801402
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-23 22:11:05 +0530

    put_io WIP

--- put_io.py
+++ put_io.py
@@ -13,31 +13,32 @@ def put_io(question_name: str, io: dict):
     os.chdir("../questions")
     try:
         os.mkdir(question_name)
-    except FileExistsError():
-        question_name = question_name.split("_")
-        if question_name[-1].isdigit:
-            question_name[-1] = str(question_name[-1] + 1)
+    except FileExistsError:
+        if question_name[-1].isdigit():
+            question_name = question_name.split("_")
+            question_name[-1] = str(int(question_name[-1]) + 1)
+            question_name = "_".join(question_name)[0]
         else:
-            question_name.append("_01")
+            question_name += "01"
 
-        question_name = "_".join(question_name)
         os.mkdir(question_name)
-    finally:
-        os.chdir(question_name)
+        print(question_name)
+
+    os.chdir(question_name)
     # Now in in a folder with question_name
 
     try:
         with open("in.txt", mode="w") as ipf:
-            ipf.write(ipf["in"])
-    except FileExistsError():
+            ipf.write(io["in"])
+    except FileExistsError:
         print("File already exists")
     finally:
         print("Made test cases inputs")
 
     try:
         with open("out.txt", mode="w") as opf:
-            opf.write(opf["out"])
-    except FileExistsError():
+            opf.write(io["out"])
+    except FileExistsError:
         print("File already exists")
     finally:
         print("Made test cases outputs")
