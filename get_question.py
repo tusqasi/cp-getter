98322e19814404dbecf7f3db53b712f309c755dc
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-23 21:00:52 +0530

    MVP reached as a whole
    make_template makes templates, put_io puts inputs and outputs, and
    get_question gets questions correctly. Maybe

--- get_question.py
+++ get_question.py
@@ -1,32 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main(URL):
-    return_obj = dict()
-    
-    return_obj['io'] = dict()
-    return_obj['io']['out'] = []
-    return_obj['io']['in'] = []
+
+def get_question(URL):
+
+    return_obj = {"URL":URL}
+    return_obj["io"] = dict()
+    return_obj["io"]["out"] = []
+    return_obj["io"]["in"] = []
 
     page = requests.get(URL).content
 
-    soup = BeautifulSoup(page,"html.parser")
+    soup = BeautifulSoup(page, "html.parser")
     parsed_items = []
 
-    for _ in soup.find_all("br" ):
-       _.replace_with("\n")
+    for _ in soup.find_all("br"):
+        _.replace_with("\n")
 
     for _ in soup.find_all("pre"):
         parsed_items.append("".join(_.contents))
 
     for _ in enumerate(parsed_items, start=1):
         if _[0] % 2 == 0:
-            return_obj['io']['out'].append(_[1])
+            return_obj["io"]["out"].append(_[1])
         else:
-            return_obj['io']['in'].append(_[1])
+            return_obj["io"]["in"].append(_[1])
 
-    return_obj["name"] = soup.find("div", class_="title").contents[0].split('.',1)[1][1:].lower()
+    return_obj["name"] = (
+        soup.find("div", class_="title").contents[0].split(".", 1)[1][1:].lower()
+    )
 
     return return_obj
+
+
 if __name__ == "__main__":
     print(main("https://codeforces.com/contest/1283/problem/B"))
--- main.py
+++ main.py
@@ -2,20 +2,6 @@ import sys
 
 TOKEN = sys.argv[1]
 
-'''
-question = {
-    'name': name,
-    'URL':  URL,
-    'io': {
-         'in': inputs,
-         'out': outputs
-    }
-'''
-
-"""
 question = get_question(TOKEN)
-
-put_tcs(question['io'])
-
-make_template(question['name'])
-"""
+put_tcs(question['name'], question['io'])
+make_template(question['URL'])
