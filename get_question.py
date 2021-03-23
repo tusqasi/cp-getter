96d384ae634f3b749e37df7e5b1bce95b58a1fd6
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-23 22:10:20 +0530

    get_question mostly works

--- get_question.py
+++ get_question.py
@@ -6,8 +6,8 @@ def get_question(URL):
 
     return_obj = {"URL":URL}
     return_obj["io"] = dict()
-    return_obj["io"]["out"] = []
-    return_obj["io"]["in"] = []
+    return_obj["io"]["out"]:str
+    return_obj["io"]["in"]:str
 
     page = requests.get(URL).content
 
@@ -22,16 +22,15 @@ def get_question(URL):
 
     for _ in enumerate(parsed_items, start=1):
         if _[0] % 2 == 0:
-            return_obj["io"]["out"].append(_[1])
+            return_obj["io"]["out"] = _[1]
         else:
-            return_obj["io"]["in"].append(_[1])
+            return_obj["io"]["in"] = _[1]
 
-    return_obj["name"] = (
-        soup.find("div", class_="title").contents[0].split(".", 1)[1][1:].lower()
-    )
+    return_obj["name"] =  soup.find("div", class_="title").contents[0].split(".", 1)[1][1:].lower()
+    
 
     return return_obj
 
 
 if __name__ == "__main__":
-    print(main("https://codeforces.com/contest/1283/problem/B"))
+    print(get_question("https://codeforces.com/contest/1283/problem/B"))
