809493e02734120df8225ebcad1b91b24cca9671
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-23 14:19:55 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+TOKEN = sys.argv[1]
+
+'''
+question = {
+    'name': name,
+    'URL':  URL,
+    'io': {
+         'in': inputs,
+         'out': outputs
+    }
+'''
+
+"""
+question = get_question(TOKEN)
+
+put_tcs(question['io'])
+
+make_template(question['name'])
+"""
