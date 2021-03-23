58ca5811a63d464cf155b7a267e5c06d877ebd47
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-23 22:09:02 +0530

    Now starting to reach MVP

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
+import get_question
+import put_io
+import make_template
 
 TOKEN = sys.argv[1]
-
-question = get_question(TOKEN)
-put_tcs(question['name'], question['io'])
-make_template(question['URL'])
+print(TOKEN)
+question = get_question.get_question(TOKEN)
+put_io.put_io(question['name'], question['io'])
+make_template.make_template(question['URL'])
