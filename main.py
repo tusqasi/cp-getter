ea778a1edb58bc7e8123f43eed8a4357a1a35b70
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-27 22:51:05 +0530

    removed print statements

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import put_io
 import make_template
 
 TOKEN = sys.argv[1]
-print(TOKEN)
 question = get_question.get_question(TOKEN)
-put_io.put_io(question['name'], question['io'])
-make_template.make_template(question['URL'])
+if put_io.put_io(question['name'], question['io']): # is true when no existsting question is found
+    make_template.make_template(question['URL'])
+print( question['name'])
