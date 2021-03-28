4fb28ca3e9e80eac2f3494cbefe234fa13b698ae
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-28 07:59:57 +0530

    clean up

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import make_template
 
 TOKEN = sys.argv[1]
 question = get_question.get_question(TOKEN)
-if put_io.put_io(question['name'], question['io']): # is true when no existsting question is found
-    make_template.make_template(question['URL'])
-print( question['name'])
+if put_io.put_io(question["name"], question["io"]):
+    make_template.make_template(question["URL"])
+print(question["name"])
