8125997832da4a810e82cad39bb75f6e2fb05f4f
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-30 20:47:09 +0530

    Chore: Error handling for url passed to main.py

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import sys
 import get_question
 import put_io
 import make_template
+def get_token():
+    try:
+        return sys.argv[1]
+    except IndexError:
+        print("No URL specified\
+             \nSpecify URL like:\
+             \npython main.py <URL>\
+             \n\n Exiting")
+        exit()
 
-TOKEN = sys.argv[1]
+TOKEN = get_token()
 question = get_question.get_question(TOKEN)
 if put_io.put_io(question["name"], question["io"]):
     make_template.make_template(question["URL"])
