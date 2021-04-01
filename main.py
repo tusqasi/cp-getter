1a0cc9da153bbcb057375749b159edda668cd467
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-04-01 15:30:55 +0530

    Feature: Json file is now able to configure things
    * `GetJsonFromFile()` gets json dispite of comments in json file. Done.
    
    * `get_config()` sets default values and configured values. WIP

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ import sys
 import get_question
 import put_io
 import make_template
+import json
+
+def GetJsonFromFile(filePath):
+    contents = ""
+    fh = open(filePath)
+    for line in fh:
+        cleanedLine = line.split("//", 1)[0]
+        if len(cleanedLine) > 0 and line.endswith("\n") and "\n" not in cleanedLine:
+            cleanedLine += "\n"
+        contents += cleanedLine
+    fh.close
+    while "/*" in contents:
+        preComment, postComment = contents.split("/*", 1)
+        contents = preComment + postComment.split("*/", 1)[1]
+    return contents
+
+def get_config():
+    try:
+        
+        config = GetJsonFromFile("~/.config/cp-getter.json")
+        print(config)
+    except FileNotFoundError:
+        print("No config file found")
+        exit()
+        
 def get_token():
     try:
         return sys.argv[1]
