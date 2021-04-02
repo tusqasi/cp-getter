58b6af8b6e9c5663aab1872975fed0811e15dd1e
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-04-02 13:52:14 +0530

    Change(Major): Now get questions from browser
    Get question, parsed and ready to use, from competitive companion

--- get_question.py
+++ get_question.py
@@ -1,37 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+question: dict
 
 
-def get_question(URL):
-
-    return_obj = {"URL": URL}
-    return_obj["io"] = dict()
-    return_obj["io"]["out"]: str
-    return_obj["io"]["in"]: str
-
-    page = requests.get(URL).content
-
-    soup = BeautifulSoup(page, "html.parser")
-    parsed_items = []
-
-    for _ in soup.find_all("br"):
-        _.replace_with("\n")
-
-    for _ in soup.find_all("pre"):
-        parsed_items.append("".join(_.contents))
-
-    for _ in enumerate(parsed_items, start=1):
-        if _[0] % 2 == 0:
-            return_obj["io"]["out"] = _[1]
-        else:
-            return_obj["io"]["in"] = _[1]
-
-    return_obj["name"] = (
-        soup.find("div", class_="title").contents[0].split(".", 1)[1][1:].lower()
-    )
-
-    return return_obj
-
+class MessageHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        length = int(self.headers.get("Content-length", 0))
+        data = self.rfile.read(length).decode()
+        question = json.loads(data)
+        httpd.shutdown()
+def main():
+    server_address = ("", 8080)
+    httpd = HTTPServer(server_address, MessageHandler)
+    httpd.serve_forever()
+    return question
 
 if __name__ == "__main__":
-    print(get_question("https://codeforces.com/contest/1283/problem/B"))
+    main()
