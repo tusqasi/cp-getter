416d6a4294bce05851a673db04c146a0b6c73703
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-04-05 13:52:46 +0530

    Change: Starting complete rewrite

--- get_question.py
+++ get_question.py
@@ -1,23 +1,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
-question: dict
+global question
 
 
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers.get("Content-length", 0))
         data = self.rfile.read(length).decode()
+        global question
+        
         question = json.loads(data)
-        httpd.shutdown()
-
+        print(question)
+        self.send_response(200)
+        self.end_headers()
+        self.flush_headers()
+        self.server.server_close()
+        print("here")
 
 def main():
     server_address = ("", 8080)
     httpd = HTTPServer(server_address, MessageHandler)
     httpd.serve_forever()
+    print("close")
     return question
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
