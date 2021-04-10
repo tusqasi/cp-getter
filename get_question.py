29e34196c043fa184b630423dcd20316d2ef2246
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-04-10 08:58:04 +0530

    Change: Working base finished for server branch

--- get_question.py
+++ get_question.py
@@ -1,30 +1,34 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+import threading
+from queue import Queue
 
 global question
 
-
+question = None
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers.get("Content-length", 0))
         data = self.rfile.read(length).decode()
         global question
-        
         question = json.loads(data)
-        print(question)
         self.send_response(200)
         self.end_headers()
         self.flush_headers()
-        self.server.server_close()
-        print("here")
 
+def not_main(s):
+    while True:
+        if (type(question) == dict):
+            s.shutdown()
+            return
 def main():
     server_address = ("", 8080)
     httpd = HTTPServer(server_address, MessageHandler)
-    httpd.serve_forever()
-    print("close")
-    return question
 
-
+    server = threading.Thread(target=httpd.serve_forever)
+    hmm = threading.Thread(target=not_main, args=(httpd,) )
+    server.start()
+    hmm.start()
+    server.join()
 if __name__ == "__main__":
-    print(main())
+    main()
